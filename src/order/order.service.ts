23b4b09a27c302161d9053b07e9c050832a6620f
riflecode <yuyashiro1208@gmail.com>
2023-08-07 13:42:02 +0800

    extract provider config to env

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,21 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import * as seaport from '@opensea/seaport-js';
-import { ethers } from 'ethers';
 import { OrderComponents } from '@opensea/seaport-js/lib/types';
+import { SeaportProvider } from 'src/provider/seaport.provider';
 
 @Injectable()
 export class OrderService {
 
-  constructor(@InjectModel('Order') private readonly orderModel) { }
+  constructor(@InjectModel('Order') private readonly orderModel, private readonly seaportProvider: SeaportProvider) { }
 
   async create(createOrderDto: CreateOrderDto) {
-    const provider = new ethers.providers.JsonRpcProvider(
-      "https://eth-sepolia.public.blastapi.io"
-    );
-    const smeSeaport = new seaport.Seaport(provider);
-    const hash = smeSeaport.getOrderHash(createOrderDto.entry.parameters as OrderComponents);
+    const hash = this.seaportProvider.getProvider().getOrderHash(createOrderDto.entry.parameters as OrderComponents);
 
     console.log('hash::', hash);
     createOrderDto.hash = hash;
