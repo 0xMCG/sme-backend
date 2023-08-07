43711119bf5996f7e6128275fbefbf495c309bac
riflecode <yuyashiro1208@gmail.com>
2023-08-07 11:44:44 +0800

    integrate seaportjs

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { InjectModel } from '@nestjs/mongoose';
+import * as seaport from '@opensea/seaport-js';
+import { ethers } from 'ethers';
+import { OrderComponents } from '@opensea/seaport-js/lib/types';
 
 @Injectable()
 export class OrderService {
@@ -8,6 +11,14 @@ export class OrderService {
   constructor(@InjectModel('Order') private readonly orderModel) { }
 
   async create(createOrderDto: CreateOrderDto) {
+    const provider = new ethers.providers.JsonRpcProvider(
+      "https://eth-sepolia.public.blastapi.io"
+    );
+    const smeSeaport = new seaport.Seaport(provider);
+    const hash = smeSeaport.getOrderHash(createOrderDto.entry.parameters as OrderComponents);
+
+    console.log('hash::', hash);
+    createOrderDto.hash = hash;
     let model = new this.orderModel(createOrderDto);
     return await model.save();
   }
