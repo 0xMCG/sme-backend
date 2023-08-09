fe0b3c8da711d1ce20213aea6ea8020cd8471378
riflecode <yuyashiro1208@gmail.com>
2023-08-09 16:34:05 +0800

    Add top collections

--- src/collection/collection.module.ts
+++ src/collection/collection.module.ts
@@ -4,9 +4,10 @@ import { CollectionController } from './collection.controller';
 import { ReservoirApi } from 'src/lib/reservoir.api';
 import { MongooseModule } from '@nestjs/mongoose';
 import { HotTopSchema } from './schema/hot.top.schema';
+import { SellingTop } from './schema/selling.top.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'HotTop', schema: HotTopSchema }])],
+  imports: [MongooseModule.forFeature([{ name: 'HotTop', schema: HotTopSchema }, { name: 'SellingTop', schema: SellingTop }])],
   controllers: [CollectionController],
   providers: [CollectionService, ReservoirApi]
 })
--- src/collection/schema/hot.top.schema.ts
+++ src/collection/schema/hot.top.schema.ts
@@ -2,138 +2,141 @@ import * as mongoose from 'mongoose';
 
 export const HotTopSchema = new mongoose.Schema({
 
-    id: String,
-     slug : String,
-     createdAt : Date,
-     name : String,
-     image : String,
-     banner : String,
-     discordUrl : String,
-     externalUrl : String,
-     twitterUsername : String,
-     openseaVerificationStatus : String,
-     description : String,
-     sampleImages : [String],
-     tokenCount : String,
-     onSaleCount : String,
-     primaryContract : String,
-     tokenSetId : String,
-     creator : String,
-     royalties : {
-         recipient : String,
-         breakdown : [
-            {
-                 bps : Number,
-                 recipient : String
-            }
-        ],
-         bps : Number
+    id: {
+        type: String,
+        index: true
     },
-     allRoyalties : {
-         eip2981 : [
+    slug: String,
+    createdAt: Date,
+    name: String,
+    image: String,
+    banner: String,
+    discordUrl: String,
+    externalUrl: String,
+    twitterUsername: String,
+    openseaVerificationStatus: String,
+    description: String,
+    sampleImages: [String],
+    tokenCount: String,
+    onSaleCount: String,
+    primaryContract: String,
+    tokenSetId: String,
+    creator: String,
+    royalties: {
+        recipient: String,
+        breakdown: [
             {
-                 bps : Number,
-                 recipient : String
+                bps: Number,
+                recipient: String
             }
         ],
-         onchain : [
+        bps: Number
+    },
+    allRoyalties: {
+        eip2981: [
             {
-                 bps : Number,
-                 recipient : String
+                bps: Number,
+                recipient: String
             }
         ],
-         opensea : [
+        onchain: [
             {
-                 bps : Number,
-                 recipient : String
+                bps: Number,
+                recipient: String
+            }
+        ],
+        opensea: [
+            {
+                bps: Number,
+                recipient: String
             }
         ]
     },
-     floorAsk : {
-         id : String,
-         sourceDomain : String,
-         price : {
-             currency : {
-                 contract : String,
-                 name :  String ,
-                 symbol :  String ,
-                 decimals : Number
+    floorAsk: {
+        id: String,
+        sourceDomain: String,
+        price: {
+            currency: {
+                contract: String,
+                name: String,
+                symbol: String,
+                decimals: Number
             },
-             amount : {
-                 raw : String,
-                 decimal : Number,
-                 usd : Number,
-                 native : Number 
+            amount: {
+                raw: String,
+                decimal: Number,
+                usd: Number,
+                native: Number
             }
         },
-         maker : String,
-         validFrom : Number,
-         validUntil : Number,
-         token : {
-             contract : String,
-             tokenId : String,
-             name : String,
-             image : String
+        maker: String,
+        validFrom: Number,
+        validUntil: Number,
+        token: {
+            contract: String,
+            tokenId: String,
+            name: String,
+            image: String
         }
     },
-     topBid : {
-         id : String,
-         sourceDomain : String,
-         price : {
-             currency : {
-                 contract : String,
-                 name : String,
-                 symbol : String,
-                 decimals : Number
+    topBid: {
+        id: String,
+        sourceDomain: String,
+        price: {
+            currency: {
+                contract: String,
+                name: String,
+                symbol: String,
+                decimals: Number
             },
-             amount : {
-                 raw : String,
-                 decimal : Number,
-                 usd : Number,
-                 native : Number
+            amount: {
+                raw: String,
+                decimal: Number,
+                usd: Number,
+                native: Number
             },
-             netAmount : {
-                 raw : String,
-                 decimal : Number,
-                 usd : Number,
-                 native : Number
+            netAmount: {
+                raw: String,
+                decimal: Number,
+                usd: Number,
+                native: Number
             }
         },
-         maker : String,
-         validFrom : Number,
-         validUntil : Number
+        maker: String,
+        validFrom: Number,
+        validUntil: Number
     },
-     rank : {
-         '1day' : Number,
-         '7day' : Number,
-         '30day' : Number,
-         allTime : Number
+    rank: {
+        '1day': Number,
+        '7day': Number,
+        '30day': Number,
+        allTime: Number
     },
-     volume : {
-        '1day' : Number,
-        '7day' : Number,
-        '30day' : Number,
-        allTime : Number
+    volume: {
+        '1day': Number,
+        '7day': Number,
+        '30day': Number,
+        allTime: Number
     },
-     volumeChange : {
-        '1day' : Number,
-        '7day' : Number,
-        '30day' : Number,
+    volumeChange: {
+        '1day': Number,
+        '7day': Number,
+        '30day': Number,
     },
-     floorSale : {
-        '1day' : Number,
-        '7day' : Number,
-        '30day' : Number,
+    floorSale: {
+        '1day': Number,
+        '7day': Number,
+        '30day': Number,
     },
-     floorSaleChange : {
-        '1day' : Number,
-        '7day' : Number,
-        '30day' : Number,
+    floorSaleChange: {
+        '1day': Number,
+        '7day': Number,
+        '30day': Number,
     },
-     collectionBidSupported : Boolean,
-     ownerCount : Number,
-     contractKind : String,
-     mintedTimestamp : String,
-     mintStages : []
+    collectionBidSupported: Boolean,
+    ownerCount: Number,
+    contractKind: String,
+    mintedTimestamp: String,
+    mintStages: []
 
 });
