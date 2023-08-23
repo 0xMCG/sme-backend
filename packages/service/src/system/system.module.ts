3c56e72357dcaed7f3dec5b2037c8090454985fc
riflecode <yuyashiro1208@gmail.com>
2023-08-23 16:53:51 +0800

    Get nft owner api

--- packages/service/src/system/system.module.ts
+++ packages/service/src/system/system.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SystemService } from './system.service';
 import { SystemController } from './system.controller';
+import { EtherProvider } from 'src/lib/ether.provider';
 
 @Module({
   controllers: [SystemController],
-  providers: [SystemService],
+  providers: [SystemService, EtherProvider],
 })
 export class SystemModule {}
