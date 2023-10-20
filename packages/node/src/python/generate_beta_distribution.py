41c0e55794ac8f039dba733d61a5b379305661be
riflecode <rifleovo@gmail.com>
2023-10-20 15:52:52 +0800

    Fix python function

--- packages/node/src/python/generate_beta_distribution.py
+++ packages/node/src/python/generate_beta_distribution.py
@@ -1,19 +1,19 @@
 from scipy.special import betaincinv
 import numpy as np
-from scipy.stats import beta as beta_a
+from scipy.stats import beta as betafuncion
 
 import sys
 
 alpha = 3
 beta = 3
-precision = 1000
+precision = 100000
 denominator = 1e4
 
 result = []
 
 def calculate(randomWord):
-    randomWord = randomWord % 1000
-    randomWord = randomWord * 1.0 / 1000
+    randomWord = randomWord % precision
+    randomWord = randomWord * 1.0 / precision
     y = betaincinv(alpha, beta, randomWord)
     y_numerator = int(y * denominator)
     y_denominator = int(denominator)
@@ -25,7 +25,7 @@ beta_params = [(2, 3, 0.9989, 0, 20),
                (3, 3, 0.001, 90, 110),
                (3, 3, 0.0001,990, 1010)]
 
-def sample_combined_beta_vrf(beta_params, vrf_number):
+def sample_combined_beta_vrf(beta_params, randomWord):
     """
     vrf_numbers: an array of tuples, where each tuple contains two uint256 numbers.
     """
@@ -33,17 +33,18 @@ def sample_combined_beta_vrf(beta_params, vrf_number):
     weight_cumsum = np.cumsum(weights)
 
     # Normalize the VRF outputs to [0, 1]
-    u1 = vrf_number / (2**256)
+    randomWord = randomWord % precision
+    randomWord = randomWord * 1.0 / precision
     
     # Step 1: Choose which Beta distribution to sample from based on the external random number u1
-    dist_idx = np.searchsorted(weight_cumsum, u1)
+    dist_idx = np.searchsorted(weight_cumsum, randomWord)
     
-    alpha, beta_param, _, a, b = beta_params[dist_idx]
+    alpha, beta_param, _, _, _ = beta_params[dist_idx]
     
     # Step 2: Perform inverse transform sampling on the chosen Beta distribution using external random number u2
-    probability = beta_a.ppf(u1, alpha, beta_param)  
+    probability = betafuncion.ppf(randomWord, alpha, beta_param)  
 
-    y_numerator = int(probability * denominator)
+    y_numerator = int(probability * 1.0 * denominator)
     y_denominator = int(denominator)
     result.append(y_numerator)
     result.append(y_denominator)
